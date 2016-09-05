000bed7a261430040e119715f2378e212bf5ab70
Spencer Gallardo <s.gallardo@criteo.com>
2016-09-05 19:53:13 -0400

    Week 2 Assignment

---
+++ task_02.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" I am adding this doc string"""
+
+print "Hello Student!"
---
+++ task_03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" I am adding this doc string"""
+
+CHARLIE = "Chocolate Factory"
+VIOLET = "Gray"
+PATRICIA = "Reichardt"
+LINUS = "van Pelt"
